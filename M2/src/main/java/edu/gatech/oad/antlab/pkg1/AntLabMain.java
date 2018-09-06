3fe77fcd686d75bcec3e1d39930ed868b0525482
An Q Tran <anqtran@gatech.edu>
2018-09-06 18:27:42 -0400

    addina person 5 name

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("nnguyen74");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
+                  Person5 p5 = new Person5("An");
                   toPrint += p5.toString("903392493");
 		  
         System.out.println(toPrint);
